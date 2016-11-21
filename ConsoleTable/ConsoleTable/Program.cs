1d96f4cf1bcf8626c3d68b0541b9a1fb1f81599a
Spreadyy <sandro@012345.ch>
2016-11-21 15:42:23 +0100

    Sample in main method

--- ConsoleTable/ConsoleTable/Program.cs
+++ ConsoleTable/ConsoleTable/Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ConsoleTable.Core;
 
 namespace ConsoleTable
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-		}
-	}
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var random = new Random();
+
+            var rowCount = 3;
+            var columnCount = 3;
+
+            var table = new double[rowCount, columnCount];
+
+            for (int row = 0; row < rowCount; row++)
+            {
+                for (int column = 0; column < columnCount; column++)
+                {
+                    table[row, column] = random.NextDouble();
+                }
+            }
+
+            var headers = new string[] { "Column 1", "Mike", "Sandro" };
+
+            var matrix = new ConsoleTable<double>(table, "Maaatrix", headers);
+            Console.WriteLine(matrix.ToString());
+            Console.ReadKey();
+        }
+    }
 }
