1ad321d4a458233ee2e56dcf11ada76e008854a0
SeboCode <mike-marti@outlook.com>
2017-02-08 00:32:19 +0100

    Moved AssignBorders functionality to TableSymbols

--- ConsoleTable/ConsoleTable.Settings/ConsoleTableSettings.cs
+++ ConsoleTable/ConsoleTable.Settings/ConsoleTableSettings.cs
@@ -1,22 +1,17 @@
-﻿using System.Collections.Generic;
-using ConsoleTable.Settings.Border;
+﻿using ConsoleTable.Settings.Border;
 using ConsoleTable.Settings.Symbols;
-using ConsoleTable.Settings.Util;
 
 namespace ConsoleTable.Settings
 {
     public class ConsoleTableSettings : IConsoleTableSettings
     {
-        private readonly Dictionary<Borders, char> _borderAssignment;
-        
         public ConsoleTableSettings(TableSymbols tableSymbols = null, bool sameRowLength = false)
         {
             if (tableSymbols == null)
             {
                 tableSymbols = new TableSymbols();
             }
-
-            _borderAssignment = new Dictionary<Borders, char>(EnumUtil.Size<HorizontalBorder>() * EnumUtil.Size<VerticalBorder>());
+            
             TableSymbols = tableSymbols;
             SameRowLength = sameRowLength;
         }
@@ -30,7 +25,6 @@ namespace ConsoleTable.Settings
             set
             {
                 _tableSymbols = value;
-                AssignBorders();
             }
         }
 
@@ -42,34 +36,7 @@ namespace ConsoleTable.Settings
         
         public char GetBorderSymbol(HorizontalBorder horizontalBorder, VerticalBorder verticalBorder)
         {
-            return _borderAssignment[new Borders(horizontalBorder, verticalBorder)];
-        }
-
-        //todo move functionality to tablesymbols
-        private void AssignBorders()
-        {
-            //todo redundant => remove
-            _borderAssignment[new Borders(HorizontalBorder.Left, VerticalBorder.Bottom)] = TableSymbols.BottomLeftCorner;
-            _borderAssignment[new Borders(HorizontalBorder.Left, VerticalBorder.Between)] =  TableSymbols.LeftRowSeperator;
-            _borderAssignment[new Borders(HorizontalBorder.Left, VerticalBorder.Top)] =  TableSymbols.TopLeftCorner;
-            _borderAssignment[new Borders(HorizontalBorder.Between, VerticalBorder.Bottom)] =  TableSymbols.BottomColumnSeperator;
-            _borderAssignment[new Borders(HorizontalBorder.Between, VerticalBorder.Between)] =  TableSymbols.TableFieldCorner;
-            _borderAssignment[new Borders(HorizontalBorder.Between, VerticalBorder.Top)] =  TableSymbols.TopColumnSeperator;
-            _borderAssignment[new Borders(HorizontalBorder.Right, VerticalBorder.Bottom)] =  TableSymbols.BottomRightCorner;
-            _borderAssignment[new Borders(HorizontalBorder.Right, VerticalBorder.Between)] =  TableSymbols.RightRowSeperator;
-            _borderAssignment[new Borders(HorizontalBorder.Right, VerticalBorder.Top)] =  TableSymbols.TopRightCorner;
-        }
-
-        private struct Borders
-        {
-            public Borders(HorizontalBorder horizontalBorder, VerticalBorder verticalBorder)
-            {
-                HorizontalBorder = horizontalBorder;
-                VerticalBorder = verticalBorder;
-            }
-
-            private HorizontalBorder HorizontalBorder { get; }
-            private VerticalBorder VerticalBorder { get; }
+            return TableSymbols.GetBorderSymbol(horizontalBorder, verticalBorder);
         }
     }
 }
--- ConsoleTable/ConsoleTable.Settings/Symbols/TableSymbols.cs
+++ ConsoleTable/ConsoleTable.Settings/Symbols/TableSymbols.cs
@@ -1,17 +1,169 @@
-﻿namespace ConsoleTable.Settings.Symbols
+﻿using System.Collections.Generic;
+using ConsoleTable.Settings.Border;
+using ConsoleTable.Settings.Util;
+
+namespace ConsoleTable.Settings.Symbols
 {
     public class TableSymbols
     {
-        public virtual char TopRightCorner { get; set; } = '┐';
-        public virtual char TopLeftCorner { get; set; } = '┌';
-        public virtual char BottomRightCorner { get; set; } = '┘';
-        public virtual char BottomLeftCorner { get; set; } = '└';
-        public virtual char RightRowSeperator { get; set; } = '┤';
-        public virtual char LeftRowSeperator { get; set; } = '├';
-        public virtual char TopColumnSeperator { get; set; } = '┬';
-        public virtual char BottomColumnSeperator { get; set; } = '┴';
-        public virtual char TableFieldCorner { get; set; } = '┼';
-        public virtual char HorizontalTableFieldBorder { get; set; } = '─';
-        public virtual char VerticalTableFieldBorder { get; set; } = '│';
+        private readonly Dictionary<Borders, char> _borderAssignment;
+
+        public TableSymbols()
+        {
+            _borderAssignment = new Dictionary<Borders, char>(EnumUtil.Size<HorizontalBorder>() * EnumUtil.Size<VerticalBorder>());
+            AssignBorders();
+        }
+
+        private char _topRightCorner = '┐';
+        public virtual char TopRightCorner
+        {
+            get { return _topRightCorner; }
+            set
+            {
+                _topRightCorner = value; 
+                AssignBorders();
+            }
+        }
+
+        private char _topLeftCorner = '┌';
+        public virtual char TopLeftCorner
+        {
+            get { return _topLeftCorner; }
+            set
+            {
+                _topLeftCorner = value;
+                AssignBorders();
+            }
+        }
+
+        private char _bottomRightCorner = '┘';
+        public virtual char BottomRightCorner
+        {
+            get { return _bottomRightCorner; }
+            set
+            {
+                _bottomRightCorner = value; 
+                AssignBorders();
+            }
+        }
+
+        private char _bottomLeftCorner = '└';
+        public virtual char BottomLeftCorner
+        {
+            get { return _bottomLeftCorner; }
+            set
+            {
+                _bottomLeftCorner = value; 
+                AssignBorders();
+            }
+        }
+
+        private char _rightRowSeperator = '┤';
+        public virtual char RightRowSeperator
+        {
+            get { return _rightRowSeperator; }
+            set
+            {
+                _rightRowSeperator = value; 
+                AssignBorders();
+            }
+        }
+
+        private char _leftRowSeperator = '├';
+        public virtual char LeftRowSeperator
+        {
+            get { return _leftRowSeperator; }
+            set
+            {
+                _leftRowSeperator = value; 
+                AssignBorders();
+            }
+        }
+
+        private char _topColumnSeperator = '┬';
+        public virtual char TopColumnSeperator
+        {
+            get { return _topColumnSeperator; }
+            set
+            {
+                _topColumnSeperator = value; 
+                AssignBorders();
+            }
+        }
+
+        private char _bottomColumnSeperator = '┴';
+        public virtual char BottomColumnSeperator
+        {
+            get { return _bottomColumnSeperator; }
+            set
+            {
+                _bottomColumnSeperator = value; 
+                AssignBorders();
+            }
+        }
+
+        private char _tableFieldCorner = '┼';
+        public virtual char TableFieldCorner
+        {
+            get { return _tableFieldCorner; }
+            set
+            {
+                _tableFieldCorner = value; 
+                AssignBorders();
+            }
+        }
+
+        private char _horizontalTableFieldBorder = '─';
+        public virtual char HorizontalTableFieldBorder
+        {
+            get { return _horizontalTableFieldBorder; }
+            set
+            {
+                _horizontalTableFieldBorder = value; 
+                AssignBorders();
+            }
+        }
+
+        private char _verticalTableFieldBorder = '│';
+        public virtual char VerticalTableFieldBorder
+        {
+            get { return _verticalTableFieldBorder; }
+            set
+            {
+                _verticalTableFieldBorder = value; 
+                AssignBorders();
+            }
+        }
+
+        public char GetBorderSymbol(HorizontalBorder horizontalBorder, VerticalBorder verticalBorder)
+        {
+            return _borderAssignment[new Borders(horizontalBorder, verticalBorder)];
+        }
+        
+        private void AssignBorders()
+        {
+            //todo redundant => remove
+            _borderAssignment[new Borders(HorizontalBorder.Left, VerticalBorder.Bottom)] = BottomLeftCorner;
+            _borderAssignment[new Borders(HorizontalBorder.Left, VerticalBorder.Between)] = LeftRowSeperator;
+            _borderAssignment[new Borders(HorizontalBorder.Left, VerticalBorder.Top)] = TopLeftCorner;
+            _borderAssignment[new Borders(HorizontalBorder.Between, VerticalBorder.Bottom)] = BottomColumnSeperator;
+            _borderAssignment[new Borders(HorizontalBorder.Between, VerticalBorder.Between)] = TableFieldCorner;
+            _borderAssignment[new Borders(HorizontalBorder.Between, VerticalBorder.Top)] = TopColumnSeperator;
+            _borderAssignment[new Borders(HorizontalBorder.Right, VerticalBorder.Bottom)] = BottomRightCorner;
+            _borderAssignment[new Borders(HorizontalBorder.Right, VerticalBorder.Between)] = RightRowSeperator;
+            _borderAssignment[new Borders(HorizontalBorder.Right, VerticalBorder.Top)] = TopRightCorner;
+        }
+
+        private struct Borders
+        {
+            public Borders(HorizontalBorder horizontalBorder, VerticalBorder verticalBorder)
+            {
+                HorizontalBorder = horizontalBorder;
+                VerticalBorder = verticalBorder;
+            }
+
+            private HorizontalBorder HorizontalBorder { get; }
+            private VerticalBorder VerticalBorder { get; }
+        }
     }
 }
