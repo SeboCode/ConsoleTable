7b8dc9dbc16b43b01d567cbe10e8155cd6e4c5cb
SeboCode <mike-marti@outlook.com>
2017-02-12 23:32:33 +0100

    Added documentation to ConsoleTable

--- ConsoleTable/ConsoleTable.Core/ConsoleTable.cs
+++ ConsoleTable/ConsoleTable.Core/ConsoleTable.cs
@@ -37,6 +37,11 @@ namespace ConsoleTable.Core
             set { this[row, column] = (T) value; }
         }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="title">The title of the table.</param>
+        /// <param name="header">The title of the columns of the table.</param>
         private ConsoleTable(string title = null, string[] header = null)
         {
             if (title.IsEmptyOrWhiteSpace())
@@ -62,6 +67,12 @@ namespace ConsoleTable.Core
         }
 
         //overwork in c# 7
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="table">The data of the table.</param>
+        /// <param name="title">The title of the table.</param>
+        /// <param name="header">The title of the columns of the table.</param>
         public ConsoleTable(T[,] table, string title = null, string[] header = null) : this(title, header)
         {
             if (table == null)
@@ -72,6 +83,13 @@ namespace ConsoleTable.Core
             _table = table;
         }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="row">The amount of rows, which the table should have.</param>
+        /// <param name="column">The amount of columns, which the table should have.</param>
+        /// <param name="title">The title of the table.</param>
+        /// <param name="header">The title of the columns of the table.</param>
         public ConsoleTable(int row, int column, string title = null, string[] header = null) : this(title, header)
         {
             if (row < 1)
@@ -86,7 +104,14 @@ namespace ConsoleTable.Core
 
             _table = new T[row, column];
         }
-        
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="rows">The data of the table.</param>
+        /// <param name="title">The title of the table.</param>
+        /// <param name="header">The title of the columns of the table.</param>
+        /// <param name="fillerElement">Value used to bring all rows to the same length.</param>
         public ConsoleTable(T[][] rows, string title = null, string[] header = null, T fillerElement = default(T)) : this(title, header)
         {
             if (rows == null)
@@ -119,14 +144,36 @@ namespace ConsoleTable.Core
             FillTable(rows, fillerElement, biggestColumn);
         }
 
+        //todo maybe move to drawer
+        /// <summary>
+        /// The Settings property gives information about how to draw the table.
+        /// </summary>
+        /// <value>The Settings property gets/sets the value of the settings object.</value>
         public IConsoleTableSettings Settings { get; set; }
 
+        /// <summary>
+        /// The Title property represents the title of the table.
+        /// </summary>
+        /// <value>The Title property gets/sets the value of the title.</value>
         public string Title { get; set; }
-
+        
+        //todo maybe remove setter
+        /// <summary>
+        /// The Header property represents the titles of the columns.
+        /// </summary>
+        /// <value>The Title property gets/sets the value of the titles of the columns.</value>
         public string[] Header { get; set; }
 
+        /// <summary>
+        /// The RowCount property represents the amount of rows of the table.
+        /// </summary>
+        /// <value>The Rowcount property gets the amount of rows of the table.</value>
         public int RowCount => _table.GetLength(0);
 
+        /// <summary>
+        /// The ColumnCount property represents the amount of rows of the table.
+        /// </summary>
+        /// <value>The ColumnCount property gets the amount of rows of the table.</value>
         public int ColumnCount => _table.GetLength(1);
 
         private void FillTable(T[][] table, T fillerElement, int biggestColumn)
